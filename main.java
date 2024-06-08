9a5d52cb76160a946983f5a9ab52db41e87245bb
e1b19080 <e1b19080@oit.ac.jp>
2024-06-08 16:27:27 +0900

    履歴１
    そもそもプログラムの形が全く分かんないんだからブランチとか意味ないやん
    うざいからmainブランチを直接更新してやる！
    (GridLayoutで表現しようとしてる．GridBagとかGroupLayoutの方がいい？)

--- main.java
+++ main.java
@@ -6,6 +6,7 @@ import java.awt.AWTEvent;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.WindowEvent;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,121 +18,32 @@ import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-class determineBoard extends JFrame{
-
-    JSpinner spinc = new JSpinner();
-    JSpinner spinr = new JSpinner();
-    JLabel labelc = new JLabel("縦：");
-    JLabel labelr = new JLabel("横：");
-    JLabel labelg = new JLabel();
-
-    class calculateGrid implements ChangeListener {
-        public void stateChanged(ChangeEvent e){
-            int c = (Integer)(spinc.getValue());
-            int r = (Integer)(spinr.getValue());
-            int g = c*r;
-            String s = String.format("マス数=%2d", g);
-            labelg.setText(s);
-        }
-    }
-
-    determineBoard(){
-        SpinnerModel modelc = new SpinnerNumberModel(3,1,9,1);
-        SpinnerModel modelr = new SpinnerNumberModel(3,1,9,1);
-        spinc.setModel(modelc);
-        spinr.setModel(modelr);
-        spinc.addChangeListener(new calculateGrid());
-        spinr.addChangeListener(new calculateGrid());
-        GridBagLayout layout = new GridBagLayout();
+//ペグソリティアをつくるクラス
+class playGame extends JFrame {
+    playGame() {
+        GridLayout layout = new GridLayout(9, 9, 0, 0);
         this.setLayout(layout);
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 4;
-        gbc.gridheight = 4;
-        layout.setConstraints(labelc,gbc);
-        add(labelc);
-        gbc.gridx = 5;
-        gbc.gridy = 0;
-        gbc.gridwidth = 4;
-        gbc.gridheight = 4;
-        layout.setConstraints(spinc,gbc);
-        add(spinc);
-        gbc.gridx = 0;
-        gbc.gridy = 5;
-        gbc.gridwidth = 4;
-        gbc.gridheight = 4;
-        layout.setConstraints(labelr,gbc);
-        add(labelr);
-        gbc.gridx = 5;
-        gbc.gridy = 5;
-        gbc.gridwidth = 4;
-        gbc.gridheight = 4;
-        layout.setConstraints(spinr,gbc);
-        add(spinr);
-        gbc.gridx = 0;
-        gbc.gridy = 9;
-        gbc.gridwidth = 6;
-        gbc.gridheight = 1;
-        layout.setConstraints(labelg,gbc);
-        add(labelg);
-        spinc.setValue(3);
+        JButton btn[] = new JButton[49];
+        for (int i = 0; i < 49; i++) {
+            btn[i] = new JButton(Integer.toString(i + 1));
+        }
+        for (int i = 0; i < 49; i++) {
+            switch (i) {
+                case 0, 1, 5, 6, 7, 8, 12, 13, 36, 37, 41, 42, 43, 44, 47, 48:
+                    add(btn[i]);
+                    break;
+                default:
+                    add(btn[i]);
+            }
+        }
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(280,150);
-        setTitle("NumUpDown");
-        setVisible(true);
+        this.setTitle("peg初期画面");
+        this.setSize(380, 380);
+        this.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new playGame();
     }
 }
-
-
-
-
-class mainPage extends JFrame{
-
-    mainPage(){
-        JLabel label = new JLabel("メニュー");
-        label.setPreferredSize(new Dimension(220,40));
-        JButton button1 = new JButton("あそぶ！");
-        button1.setPreferredSize(new Dimension(220,40));
-        JButton button2 = new JButton("じどう！");
-        button2.setPreferredSize(new Dimension(220,40));
-
-        button1.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                label.setText("勉強しろ");
-            }
-        });
-        button2.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-               new determineBoard(); 
-                setVisible(false);
-            }
-        });
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        panel.add(label, BorderLayout.NORTH);
-        panel.add(button1,BorderLayout.CENTER);
-        panel.add(button2,BorderLayout.SOUTH);
-        add(panel);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("ペグソリティア");
-        pack();
-        setVisible(true);
-
-    }
-
-
-
-
-    public static void main(String[] args){
-        new mainPage();
-    }
-
-}
-
-
-
-
